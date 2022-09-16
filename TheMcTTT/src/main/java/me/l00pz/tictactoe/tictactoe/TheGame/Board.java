8de2c667a0c86f078478afaaa276acc7fade9c95
AmirAli Seyedzadegan <mahbodseyedzadegan@gmail.com>
2022-09-16 22:50:29 +0430

    Update Board.java

--- TheMcTTT/src/main/java/me/l00pz/tictactoe/tictactoe/TheGame/Board.java
+++ TheMcTTT/src/main/java/me/l00pz/tictactoe/tictactoe/TheGame/Board.java
@@ -11,35 +11,6 @@ public class Board {
         this.board = board;
     }
 
-    public void defaultBoard(){
-        System.out.println("| - | - | - |");
-        System.out.println("| 1 | 2 | 3 |");
-        System.out.println("| - | - | - |");
-        System.out.println("| 4 | 5 | 6 |");
-        System.out.println("| - | - | - |");
-        System.out.println("| 7 | 8 | 9 |");
-        System.out.println("| - | - | - |");
-    }
-    public void drawer(){
-        System.out.println("| - | - | - |");
-        System.out.println(printRow (0));
-        System.out.println("| - | - | - |");
-        System.out.println(printRow (1));
-        System.out.println("| - | - | - |");
-        System.out.println(printRow (2));
-        System.out.println("| - | - | - |");
-    }
-    private String printRow(int row){
-        StringBuilder rowMaker = new StringBuilder("| ");
-        for (int i=0;i<3;i++){
-            if(board[row][i] == 0) rowMaker.append(" ");
-            if(board[row][i] == 1) rowMaker.append("X");
-            if(board[row][i] == 2) rowMaker.append("O");
-            rowMaker.append(" | ");
-        }
-        rowMaker.deleteCharAt(rowMaker.lastIndexOf(" "));
-        return rowMaker.toString();
-    }
     public boolean placed (int pos){
         int row = (pos - 1)/3;
         int col = (pos - (row*3))-1;
